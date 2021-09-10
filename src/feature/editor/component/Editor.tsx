4f5e94a7ade792d6253eac1e6e96112b30bbedf6
Joseph R Miles <joe@jrm.dev>
2021-09-10 12:23:55 -0700

    feat: add grid to editor component

--- src/feature/editor/component/Editor.tsx
+++ src/feature/editor/component/Editor.tsx
@@ -1,15 +1,46 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 
 import useLocalStore from '@/store/local'
-import { toSvgMatrix } from '@/lib/Transform'
+import { getScale, inverseOf, toSvgMatrix } from '@/lib/Transform'
 
 import { Table } from '@/feature/table'
 
-const Grid = (): JSX.Element => {
+const GridPattern = forwardRef<SVGPatternElement>((_, ref): JSX.Element => {
   const viewTransform = useLocalStore(({ viewTransform }) => viewTransform)
 
   return (
-    <rect transform={toSvgMatrix(viewTransform)} width='10' height='10' />
+    <defs>
+      <pattern
+        ref={ref}
+        id='rectangular-grid-pattern'
+        width='16'
+        height='16'
+        patternUnits='userSpaceOnUse'
+        patternTransform={
+          viewTransform !== undefined
+            ? toSvgMatrix(viewTransform)
+            : ''
+        }
+      >
+        <path
+          stroke='lightgrey'
+          fill='none'
+          d='M 16,0 L 0,0 0,16 16,16'
+          strokeWidth={1 * getScale(inverseOf(viewTransform))[0]}
+        />
+      </pattern>
+    </defs>
+  )
+})
+
+const Grid = (): JSX.Element => {
+  const ref = React.useRef<SVGPatternElement>(null)
+
+  return (
+    <>
+      <GridPattern ref={ref} />
+      <rect width='100%' height='100%' fill={`url(#${ref.current?.id ?? ''})`} />
+    </>
   )
 }
 
