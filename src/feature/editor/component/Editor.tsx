30fc4705db51381f4115b4dd544fbda4c99062d7
Joseph R Miles <joe@jrm.dev>
2021-09-10 08:22:34 -0700

    feat: editor grid uses view transform

--- src/feature/editor/component/Editor.tsx
+++ src/feature/editor/component/Editor.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+
+import useLocalStore from '@/store/local'
+import { toSvgMatrix } from '@/lib/Transform'
+
 import { Table } from '@/feature/table'
 
-const Grid = (): JSX.Element => (
-  <rect />
-)
+const Grid = (): JSX.Element => {
+  const viewTransform = useLocalStore(({ viewTransform }) => viewTransform)
+
+  return (
+    <rect transform={toSvgMatrix(viewTransform)} width='10' height='10' />
+  )
+}
 
 export const Editor = (): JSX.Element => (
   <Table background={Grid} />
