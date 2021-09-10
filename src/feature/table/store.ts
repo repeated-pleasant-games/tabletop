e54c05affbbf72e203f9c0c588929d6991d2bcda
Joseph R Miles <joe@jrm.dev>
2021-09-10 08:13:12 -0700

    feat: add setViewTransformTo
    Allows viewTransform to be snapped to a position.

--- src/feature/table/store.ts
+++ src/feature/table/store.ts
@@ -1,9 +1,18 @@
-import { identityTransform, Transform } from '@/lib/Transform'
+import { SetState } from 'zustand'
+import { identityTransform, Transform, transformOf } from '@/lib/Transform'
 
 interface TableStateSlice {
   viewTransform: Transform
+  setViewTransformTo: (pos: [ x: number, y: number ], scale: number) => void
 }
 
-export const createTableStateSlice = (): TableStateSlice => ({
-  viewTransform: identityTransform()
+export const createTableStateSlice = (
+  set: SetState<TableStateSlice>
+): TableStateSlice => ({
+  viewTransform: identityTransform(),
+  setViewTransformTo: ([x, y], scale) =>
+    set((prevState) => ({
+      ...prevState,
+      viewTransform: transformOf(x, y, scale)
+    }))
 })
