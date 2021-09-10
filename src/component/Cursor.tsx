ef4c6215299aae953d539132678270677cd1a137
Joseph R Miles <joe@jrm.dev>
2021-09-10 12:34:31 -0700

    feat: add cursor component

---
+++ src/component/Cursor.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+export const Cursor = ({
+  x,
+  y,
+  name,
+  fill
+}: {
+  x: number
+  y: number
+  name?: string
+  fill: string
+}): JSX.Element => (
+  <g
+    transform={`translate(${x},${y})`}
+    style={{
+      fill
+    }}
+  >
+    <g
+      transform='
+        scale(0.025)
+        matrix(1.82605,-0.803689,0.803689,1.82605,-531.498,-59.7769)
+      '
+    >
+      <path
+        d='
+          M 120.099,388.586
+          L 231.762,134.739
+          L 343.424,388.586
+          L 231.762,355.867
+          L 120.099,388.586
+          Z
+        '
+      />
+    </g>
+    {
+      name !== undefined
+        ? (
+          <text x={10} y={24}>
+            {name}
+          </text>
+        )
+        : null
+    }
+  </g>
+)
