1fd9bb985e78331ce252007103c2685b794a6a86
Joseph R Miles <joe@jrm.dev>
2022-02-26 07:33:34 -0800

    style: grid size is same as roll20

--- src/feature/table/component/RectangularGrid.tsx
+++ src/feature/table/component/RectangularGrid.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import useLocalStore from '@/store/local'
 import { getScale, inverseOf, toSvgMatrix } from '@/lib/Transform'
 
+const GRID_CELL_WIDTH = 70 // Roll20 uses a 70px grid. 70px ~ 1in
+
 export const RectangularGrid = (): JSX.Element => {
   const viewTransform = useLocalStore(({ viewTransform }) => viewTransform)
 
@@ -12,15 +14,15 @@ export const RectangularGrid = (): JSX.Element => {
       <defs>
         <pattern
           id={patternId}
-          width='16'
-          height='16'
+          width={GRID_CELL_WIDTH}
+          height={GRID_CELL_WIDTH}
           patternUnits='userSpaceOnUse'
           patternTransform={toSvgMatrix(viewTransform)}
         >
           <path
             stroke='lightgrey'
             fill='none'
-            d='M 16,0 L 0,0 0,16 16,16'
+            d={`M ${GRID_CELL_WIDTH},0 L 0,0 0,${GRID_CELL_WIDTH} ${GRID_CELL_WIDTH},${GRID_CELL_WIDTH}`}
             strokeWidth={1 * getScale(inverseOf(viewTransform))[0]}
           />
         </pattern>
