4c1b2e807bca76d72e3f404641ed44b467bd4708
Joseph R Miles <joe@jrm.dev>
2022-02-27 14:47:22 -0800

    refactor: extracted inital x and y as variables

--- src/feature/table/feature/token/component/Token.tsx
+++ src/feature/table/feature/token/component/Token.tsx
@@ -4,11 +4,14 @@ import useLocalStore from '@/store/local'
 import { toSvgMatrix } from '@/lib/Transform'
 
 export const Token = (): JSX.Element => {
+  const x = 0
+  const y = 0
+
   const viewTransform = useLocalStore(({ viewTransform }) => viewTransform)
 
   return (
     <rect
-      x={0} y={0}
+      x={x} y={y}
       width='70' height='70'
       transform={toSvgMatrix(viewTransform)}
     />
