f18e9c42214e9b67cf5a5d497cda1ce4c817bb0c
Joseph R Miles <joe@jrm.dev>
2022-05-01 14:12:48 -0700

    refactor: table uses useDrag hook

--- src/feature/table/component/Table.tsx
+++ src/feature/table/component/Table.tsx
@@ -2,17 +2,18 @@ import React from 'react'
 
 import useLocalStore from '@/store/local'
 import { scaleBy, translateBy } from '@/lib/Transform'
+import orchestrate from '@/lib/orchestrate'
 import { Cursor } from '@/component/Cursor'
 
 import { Token } from '../feature/token'
 import { useScroll } from '../hook/useScroll'
+import { useDrag } from '../hook/useDrag'
 
 export const Table = ({
   background: Background
 }: {
   background: () => JSX.Element
 }): JSX.Element => {
-  const capturedPointerId = React.useRef<number>(-1)
   const prevPointerPosition = React.useRef<[x: number, y: number]>([0, 0])
   const [pointerType, setPointerType] =
     React.useState<'mouse' | 'pen' | 'touch'>('mouse')
@@ -26,7 +27,34 @@ export const Table = ({
       [viewTransform, setViewTransform]
   )
 
-  const { scrollY, eventListeners } = useScroll()
+  const {
+    pointerPosition: [dragX, dragY],
+    pointerType: dragPointerType,
+    eventListeners: dragEventListeners
+  } = useDrag()
+
+  React.useEffect(
+    () => {
+      setViewTransform(
+        translateBy(
+          [
+            dragX - prevPointerPosition.current[0],
+            dragY - prevPointerPosition.current[1]
+          ],
+          viewTransform
+        )
+      )
+
+      prevPointerPosition.current = [dragX, dragY]
+    },
+    [dragX, dragY]
+  )
+
+  React.useEffect(() => {
+    setPointerType(dragPointerType)
+  }, [dragPointerType])
+
+  const { scrollY, eventListeners: scrollEventListeners } = useScroll()
 
   React.useEffect(
     () => {
@@ -51,35 +79,18 @@ export const Table = ({
         (target as Element).releasePointerCapture(pointerId)
         setShowPointer(false)
       }}
-      onPointerDown={({ pointerId, target, clientX, clientY }) => {
-        capturedPointerId.current = pointerId
-        prevPointerPosition.current = [clientX, clientY]
-
-        ;(target as Element).setPointerCapture(pointerId)
-      }}
-      onPointerMoveCapture={({ clientX, clientY, pointerType }) => {
-        setPointerType(pointerType)
-        setPointerPosition([clientX, clientY])
-
-        if (capturedPointerId.current !== -1) {
-          setViewTransform(
-            translateBy(
-              [
-                clientX - prevPointerPosition.current[0],
-                clientY - prevPointerPosition.current[1]
-              ],
-              viewTransform
-            )
-          )
-
-          prevPointerPosition.current = [clientX, clientY]
-        }
-      }}
-      onPointerUp={({ pointerId, target }) => {
-        capturedPointerId.current = -1
-        ;(target as Element).releasePointerCapture(pointerId)
-      }}
-      {...eventListeners}
+      onPointerMoveCapture={orchestrate(
+        (e) => setPointerPosition([e.clientX, e.clientY]),
+        dragEventListeners.onPointerMoveCapture
+      )}
+      // eslint-disable-next-line react/jsx-handler-names
+      onPointerDown={orchestrate(
+        (e) => { prevPointerPosition.current = [e.clientX, e.clientY] },
+        dragEventListeners.onPointerDown
+      )}
+      // eslint-disable-next-line react/jsx-handler-names
+      onPointerUp={dragEventListeners.onPointerUp}
+      {...scrollEventListeners}
     >
       <Background />
       <Token />
