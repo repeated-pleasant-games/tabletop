a075b643702b76a4cc7c9d8c2c32f3ee1e22ac4a
Joseph R Miles <joe@jrm.dev>
2022-05-01 15:47:35 -0700

    feat: tokens account for cursor position on drag

--- src/feature/table/feature/token/component/Token.tsx
+++ src/feature/table/feature/token/component/Token.tsx
@@ -4,13 +4,18 @@ import useLocalStore from '@/store/local'
 import { inverseOf, apply, toSvgMatrix } from '@/lib/Transform'
 
 import { useDrag } from '@/feature/table/hook/useDrag'
+import orchestrate from '@/lib/orchestrate'
 
 export const Token = (): JSX.Element => {
   const [[x, y], setPosition] = React.useState<[number, number]>([0, 0])
+  const [[deltaX, deltaY], setDelta] = React.useState<[number, number]>([0, 0])
 
   const {
     pointerPosition: [pointerX, pointerY],
-    eventListeners
+    eventListeners: {
+      onPointerDown,
+      ...eventListeners
+    }
   } = useDrag()
 
   const viewTransform = useLocalStore(({ viewTransform }) => viewTransform)
@@ -20,7 +25,7 @@ export const Token = (): JSX.Element => {
       setPosition(
         apply(
           inverseOf(viewTransform),
-          [pointerX, pointerY]
+          [pointerX + deltaX, pointerY + deltaY]
         )
       )
     },
@@ -32,6 +37,17 @@ export const Token = (): JSX.Element => {
       x={x} y={y}
       width='70' height='70'
       transform={toSvgMatrix(viewTransform)}
+      onPointerDown={orchestrate(
+        onPointerDown,
+        ({ clientX, clientY }) => {
+          const [screenX, screenY] = apply(viewTransform, [x, y])
+
+          setDelta([
+            screenX - clientX,
+            screenY - clientY
+          ])
+        }
+      )}
       {...eventListeners}
     />
   )
